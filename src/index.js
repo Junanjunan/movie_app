15def0605383252bca563fccc401da62ab27015e
unknown <jjj1305@hanmail.net>
2021-06-15 00:49:35 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Potato from './Potato';
-import Food from './Food';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App /><Potato />
-    <Food fav="kimchi"/>
-    <Food fav="pizza"/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
